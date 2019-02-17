d2763b2d61990c682889c17296f61bb70556865b
richard <r.nguyen.206@gmail.com>
2019-02-17 09:34:04 -0700

    Ditching the point insertion in writeGCode because it's threaded

--- src/LayerPlan.cpp
+++ src/LayerPlan.cpp
@@ -1393,6 +1393,66 @@ void LayerPlan::writeGCode(GCodeExport& gcode)
 
         bool update_extrusion_offset = true;
 
+        // handle split if fiber extrusion is enabled 
+        // the next path has to be a travel
+        for(unsigned int path_idx = 0; path_idx < paths.size(); path_idx++)
+        {
+            auto path = paths[path_idx];
+            bool split = false;
+            if (path_idx != paths.size() - 1 && paths[path_idx + 1].isTravelPath())
+            {
+                split = true;
+                float dist = 0;
+                float previous_dist = 0;
+                std::vector<Point> points;
+
+                //generate a flat list of points
+                for(int idx = path_idx; idx >= 0; idx--)
+                {
+                    for(int inner_idx = paths[idx].points.size() - 1; inner_idx >= 0; inner_idx--)
+                    {
+                        points.push_back(paths[idx].points[inner_idx]);
+                    }
+                }
+
+                //find the split point
+                int split_idx = points.size() - 1;
+                Point forward_point, back_point;
+                for(int split_index ; (dist < fiber_cut_length) && split_idx > 0; split_idx--)
+                {
+                    float seg_length;
+                    forward_point = points[split_idx-1];
+                    back_point = points[split_idx];
+
+                    seg_length = cylSize(back_point, forward_point, z);
+                    
+                    dist += seg_length;
+                    if (dist < fiber_cut_length)
+                    {
+                        previous_dist += seg_length;
+                    }
+                }
+
+                assert(dist > previous_dist); 
+
+                //look for the forward point while maintaining list structure
+                for(int idx = path_idx; idx >= 0; idx--)
+                {
+                    for(int inner_idx = paths[idx].points.size() - 1; inner_idx >= 0; inner_idx--)
+                    {
+                        if(paths[idx].points[inner_idx] == forward_point)
+                        {
+                            coord_t to_trim = dist - previous_dist;
+                            Point pt = cylSurfaceLerp(to_trim, back_point, forward_point, z);
+                            path.points.insert(path.points.begin()+inner_idx, pt);
+                            gcode.writeComment("inserting...");
+                            log("path_idx: %d \n", path_idx);
+                        }
+                    }
+                }
+            }
+        }
+
         for(unsigned int path_idx = 0; path_idx < paths.size(); path_idx++)
         {
             extruder_plan.handleInserts(path_idx, gcode);
@@ -1504,78 +1564,11 @@ void LayerPlan::writeGCode(GCodeExport& gcode)
                     }
                     else 
                     {
-                        // handle split if fiber extrusion is enabled 
-                        // the next path has to be a travel
-                        bool split = false;
-                         if (path_idx != paths.size() - 1 && paths[path_idx + 1].isTravelPath())
+                        for(int point_idx = 0; point_idx < path.points.size(); point_idx++)
                         {
-                            split = true;
-                            float dist = 0;
-                            float previous_dist = 0;
-                            std::vector<Point> points;
-
-                            //generate a flat list of points
-                            for(int idx = path_idx; idx >= 0; idx--)
-                            {
-                                for(int inner_idx = paths[idx].points.size() - 1; inner_idx >= 0; inner_idx--)
-                                {
-                                    points.push_back(paths[idx].points[inner_idx]);
-                                }
-                            }
-
-                            //find the split point
-                            int split_idx = points.size() - 1;
-                            Point forward_point, back_point;
-                            for(int split_index ; (dist < fiber_cut_length) && split_idx > 0; split_idx--)
-                            {
-                                float seg_length;
-                                forward_point = points[split_idx-1];
-                                back_point = points[split_idx];
-
-                                seg_length = cylSize(back_point, forward_point, z);
-                                
-                                dist += seg_length;
-                                if (dist < fiber_cut_length)
-                                {
-                                    previous_dist += seg_length;
-                                }
-                            }
-
-                            assert(dist > previous_dist); 
-
-                            //look for the forward point while maintaining list structure
-                            for(int idx = path_idx; idx >= 0; idx--)
-                            {
-                                for(int inner_idx = paths[idx].points.size() - 1; inner_idx >= 0; inner_idx--)
-                                {
-                                    if(paths[idx].points[inner_idx] == forward_point)
-                                    {
-                                        coord_t to_trim = dist - previous_dist;
-                                        Point pt = cylSurfaceLerp(to_trim, back_point, forward_point, z);
-                                        path.points.insert(path.points.begin()+inner_idx, pt);
-                                        gcode.writeComment("inserting...");
-                                    }
-                                }
-                            }
-
-                            // handle the gcode writing separately
-                            for(int point_idx = 0; point_idx < path.points.size(); point_idx++)
-                            {
-                                sendLineTo(path.config->type, path.points[point_idx], path.getLineWidthForLayerView(), path.config->getLayerThickness(), speed);
-                                if (path.points[point_idx] == forward_point)
-                                {
-                                    gcode.writeComment("SPLIT");
-                                }
-                                gcode.writeExtrusion(path.points[point_idx], speed, path.getExtrusionMM3perMM(), path.config->type, update_extrusion_offset, train->getSettingInMicrons("drum_radius"));
-                            }
-                        }
-                        else
-                        {
-                            for(int point_idx = 0; point_idx < path.points.size(); point_idx++)
-                            {
-                                sendLineTo(path.config->type, path.points[point_idx], path.getLineWidthForLayerView(), path.config->getLayerThickness(), speed);
-                                gcode.writeExtrusion(path.points[point_idx], speed, path.getExtrusionMM3perMM(), path.config->type, update_extrusion_offset, train->getSettingInMicrons("drum_radius"));
-                            }
+                            //gcode.writeComment("NORMAL");
+                            sendLineTo(path.config->type, path.points[point_idx], path.getLineWidthForLayerView(), path.config->getLayerThickness(), speed);
+                            gcode.writeExtrusion(path.points[point_idx], speed, path.getExtrusionMM3perMM(), path.config->type, update_extrusion_offset, train->getSettingInMicrons("drum_radius"));
                         }
                     }
                 }
